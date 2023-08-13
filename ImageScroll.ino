1cda0e17484148456794d466a941ffa23ba93492
Patrick Waugh <patrick.waugh@icloud.com>
2023-08-12 22:06:19 -0500

    Updated for T-Display S3
    Personalized photo to experiment, as well as update for the LilyGo T-Display S3 for ESP32-S3 1.9" ST7789 320x170 display.

--- ImageScroll.ino
+++ ImageScroll.ino
@@ -1,57 +1,50 @@
 #include "TFT_eSPI.h"
-#include "image.h"
-TFT_eSPI tft= TFT_eSPI();
+#include "picture2.h"
+
+TFT_eSPI tft = TFT_eSPI();
 TFT_eSprite sprite = TFT_eSprite(&tft);
 
+int imageW = 480;
+int imageH = 296;
 
-int imageW=480;
-int imageH=270;
+int screenW = 320;
+int screenH = 170;
+int m = imageW;
 
-int screenW=240;
-int screenH=135;
-int m=imageW;
-
-int start=1;
-unsigned short imageS[32400]={0}; // edit this screenW * screen H
-
+int start = 1;
+unsigned short imageS[142080] = { 0 };  // edit this screenW * screen H
 
 void setup() {
-tft.init();
-tft.setRotation(1);
-tft.setSwapBytes(true);
-tft.fillScreen(TFT_BLACK);
-
+  tft.init();
+  tft.setRotation(1);
+  tft.setSwapBytes(true);
+  tft.fillScreen(TFT_BLACK);
 }
-int pos=0;
-int x=0;
-int y=30;
-int changeX=1;
-int changeY=1;
+int pos = 0;
+int x = 0;
+int y = 30;
+int changeX = 1;
+int changeY = 1;
 void loop() {
 
-  pos=x+imageW*y;
-  start=pos;
-  m=screenW+pos;
-  for(int i=0;i<screenW*screenH;i++)
-  {
-  if(start%m==0)
-  {
-  start=start+(imageW-screenW);
-  m=m+imageW;
+  pos = x + imageW * y;
+  start = pos;
+  m = screenW + pos;
+  for (int i = 0; i < screenW * screenH; i++) {
+    if (start % m == 0) {
+      start = start + (imageW - screenW);
+      m = m + imageW;
+    }
+    imageS[i] = picture2[start];
+    start++;
   }
-  imageS[i]=picture[start];
-  start++;
-  
-  }
-  x=x+changeX;
-  if(x==imageW-screenW-1 || x<0)
-  changeX=changeX*-1;
+  x = x + changeX;
+  if (x == imageW - screenW - 1 || x < 0)
+    changeX = changeX * -1;
 
-   y=y+changeY;
-  if(y==imageH-screenH-1 || y<0)
-  changeY=changeY*-1;
-  
-  tft.pushImage(0,0,screenW,screenH,imageS);
-  
+  y = y + changeY;
+  if (y == imageH - screenH - 1 || y < 0)
+    changeY = changeY * -1;
 
+  tft.pushImage(0, 0, screenW, screenH, imageS);
 }
